1e7303e6ef722d8bc810888032f3306259d8c131
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-12 16:41:58 -0300

    Teoria2

---
+++ Operadores.js
@@ -0,0 +1,21 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+var nombre = 'CAMILO';
+var saludo = 'HOLA ';
+console.log(saludo + nombre)
