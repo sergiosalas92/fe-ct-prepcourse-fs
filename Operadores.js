1417507e10e2e77532aa8703200c064efc3eb8b3
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-12 16:43:20 -0300

    Teoria4

--- Operadores.js
+++ Operadores.js
@@ -19,3 +19,30 @@ console.log(3 + 5 * 2 - 8);
 var nombre = 'CAMILO';
 var saludo = 'HOLA ';
 console.log(saludo + nombre)
+
+//Igual (==)	Devuelve true si los operandos son iguales.
+//No es igual (!=)	Devuelve true si los operandos no son iguales.
+//Estrictamente igual (===)	Devuelve true si los operandos son iguales y del mismo tipo. Consulta también Object.is y similitud en JS.
+//Desigualdad estricta (!==)	Devuelve true si los operandos son del mismo tipo pero no iguales, o son de diferente tipo.
+//Mayor que (>)	Devuelve true si el operando izquierdo es mayor que el operando derecho.
+//Mayor o igual que (>=)	Devuelve true si el operando izquierdo es mayor o igual que el operando derecho.
+//Menor que (<)	Devuelve true si el operando izquierdo es menor que el operando derecho.
+//Menor o igual (<=)	Devuelve true si el operando izquierdo es menor o igual que el operando derecho.
+
+//Operaciones de comparación
+console.log (4 < 7); 
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+//Igualdad vs igualdad estricta 
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
+//Asignación y asociatividad
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log (c); 
