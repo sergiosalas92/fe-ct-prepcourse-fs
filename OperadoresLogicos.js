3909c2816e7e6626757df4cd942f7774749343b8
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-13 15:24:58 -0300

    s

--- OperadoresLogicos.js
+++ OperadoresLogicos.js
@@ -1,11 +1,18 @@
-//AND
+//AND 
+// En una declaración &&, si la primera expresión es false, la segunda expresión 
+// no se evaluará porque AMBAS expresiones deben ser true. 
+
 function mayorYMenorYPar(num) {
 	if( num > 5 && num < 10  && num  % 2 === 0 ) console.log(true);
 	else console.log(false);
 };
 mayorYMenorYPar(7);
 mayorYMenorYPar(8); 
+
 // OR
+// Si la primera expresión es verdadero, la segunda no se evaluará porque solo 
+// debe haber una declaración verdadera para cumplir con los requisitos del operador. 
+
 function operadorOr(str) {
    if (str === 'Henry' || str.length < 2) console.log(true);
    else console.log(false);
@@ -13,7 +20,12 @@ function operadorOr(str) {
 operadorOr('Henry');
 operadorOr('Javascript');
 operadorOr('H');
+
 // NOT
+// utilizamos paréntesis para evaluar PRIMERO lo que estaba dentro de ellos, y 
+// luego aplicamos la negación. Podemos ajustar cualquier expresión entre 
+// paréntesis y se evaluará antes que toda la expresión en conjunto.
+
 function negacion(permiso) {
    if (permiso) console.log('Tiene permiso');
 }
