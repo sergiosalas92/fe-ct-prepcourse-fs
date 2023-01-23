2291aea5a0abc4297cefa7bb3507264767f93e28
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-23 17:59:32 -0300

    77

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -90,13 +90,13 @@ function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
-   var max = numeros [0];
-   for (var i = 0; i < arrayOfNums.length; i++) {
-      if (numeros [i] > max) {
-         max = numeros [i];
-      }
+   var maximo = arrayOfNums[0];
+   for (var i = 1; i < arrayOfNums.length; i++) {
+     if(arrayOfNums[i] > maximo) { 
+       maximo = arrayOfNums[i];
+     }
    }
-   return max;
+   return maximo;
 }
 
 function multiplicarArgumentos() {
