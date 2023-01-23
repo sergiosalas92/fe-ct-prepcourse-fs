83174f74a56d2c94fa9e434d40c53a06a98255b1
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-23 13:56:28 -0300

    subiendo

--- Arrays.js
+++ Arrays.js
@@ -10,3 +10,16 @@ console.log(elementoDelArray);
 // Lenght
 var nombres = ['Matias', 'Maria', 'Diego', 'Rosa'].length;
 console.log(nombres);
+
+// Push y Pop
+// POP = elimina el ultimo elemento de la matriz, disminuyendo la longitud en 1 
+// .pop devuelve el elemento "reventado" (popped).
+// Push = agrega un elemento al final de la matruz, incrementado su longitud en  1 
+// .push devuelve la nueva longitud
+
+// UNSHIFT y SHIFT
+// .unshift(item) colocara el nuevo elemento en la primera posicion de la matriz
+// .shift() eliminara el primer elemento de la matriz
+
+// LENGTH
+// .length cuenta los caracteres, la matriz, devolvera el numero de elementos en una matriz.
