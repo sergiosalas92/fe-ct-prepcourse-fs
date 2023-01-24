ee85d55a76eb22f15f76594ea7db065e81e7d5fb
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-24 18:52:14 -0300

    asd

---
+++ Objetos.js
@@ -0,0 +1,5 @@
+var deportes = {
+    conBalon: ['Futbol', 'Basketball', 'Golf'],
+    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+ };
+ var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
