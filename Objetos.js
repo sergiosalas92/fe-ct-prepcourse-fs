b71ca5e289eebc61f471a2d4d78462cb54ab2213
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-25 12:50:26 -0300

    op

--- Objetos.js
+++ Objetos.js
@@ -3,3 +3,18 @@ var deportes = {
     sinBalon: ['Boxeo', 'Surf', 'Trekking'],
  };
  var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+ 
+ // ACCEDER
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+console.log(persona.edad);
+
+// ASIGNAR
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+persona.nombre = 'Martín';
+console.log(persona.nombre);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
+
