2b758cc5736c77291f86b26b4d3e6d1eda590f6a
sergiosalas92 <sergiosalas92@hotmail.com>
2023-01-31 19:20:45 -0300

    dasdda

--- Clases.js
+++ Clases.js
@@ -1,6 +1,7 @@
 //Las clases de JavaScript proveen una sintaxis mucho más clara y simple para 
 //crear objetos y lidiar con la herencia.
 //Tenemos dos maneras de declararlas...
+
 // FUNCIÓN CONSTRUCTORA
 function Auto(puertas, color, marca, año, ruedas) {
     this.puertas = puertas;
@@ -12,6 +13,7 @@ function Auto(puertas, color, marca, año, ruedas) {
  let miPrimerAuto = new Auto(2, 'Rojo', 'Ferrari', 2018, 4);
  console.log(miPrimerAuto);
  console.log(miPrimerAuto.marca);
+
  // EXPRESIÓN DE CLASE
  class Auto {
     constructor(puertas, color, marca, año, ruedas) {
@@ -25,3 +27,46 @@ function Auto(puertas, color, marca, año, ruedas) {
  let miSegundoAuto = new Auto(4, 'Blanco', 'Fiat', 2015, 4);
  console.log(miSegundoAuto);
  console.log(miSegundoAuto.marca);
+
+ // EXTENSIONES
+
+ //Extender clases nos va a permitir tener una clase general que le heredará 
+ //propiedades y métodos a otras clases. De esta forma, podremos generar 
+ //distintas sub-entidades que puedan mantener sus propias características, 
+ //pero que compartan algunas otras generales.
+
+ class Persona {
+    constructor(nombre, edad) {
+       this.nombre = nombre;
+       this.edad = edad;
+    }
+    saludar() {
+       console.log(
+          'Hola, mi nombre es  ' + this.nombre + '. Tengo  ' + this.edad
+       );
+    }
+ }
+ let martin = new Persona('Martin', 26);
+ martin.saludar();
+ class Programador extends Persona {
+    constructor(nombre, edad, añosDeExperiencia) {
+       super(nombre, edad);
+       this.añosDeExperiencia = añosDeExperiencia;
+    }
+    codear() {
+       console.log(
+          'Soy ' +
+             this.nombre +
+             ' . Codeo desde hace  ' +
+             this.añosDeExperiencia +
+             ' años'
+       );
+    }
+ }
+ 
+ let Martin = new Persona('Martín', 26);
+ let programador = new Programador('María', 37, 4);
+ martin.saludar();
+ programador.codear();
+
+
